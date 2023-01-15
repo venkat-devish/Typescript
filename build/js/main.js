e654536735df102d22055830033b29a6970f458c
venkat-devish <venkatsuryakiranv@gmail.com>
2023-01-15 23:51:36 +0530

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let myName;
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "Dave";
+meaningOfLife = 22;
+isLoading = true;
+album = 1999;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+let myName: string;
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "Dave";
+meaningOfLife = 22;
+isLoading = true;
+album = 1999;
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postId: string | number;
