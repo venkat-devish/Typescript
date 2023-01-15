fa4b513e028efab1e86536cf97a772aac9e4c58c
venkat-devish <venkatsuryakiranv@gmail.com>
2023-01-16 00:27:24 +0530

    add: Data Types

---
+++ build/js/DataTypes.js
@@ -0,0 +1,16 @@
+"use strict";
+//Arrays
+let stringArr = ["one", "hey", "Venkat"];
+let guitars = ["Strat", "VVSISHH", 5150];
+let mixedData = ["EVH", 1984, true];
+stringArr.push("Hiii");
+stringArr[0] = "Sundar";
+guitars[0] = 1984;
+guitars.unshift("Yamaha");
+let test = [];
+let bands = [];
+bands.push("Glinx");
+console.log(guitars);
+//Tuples
+let myTuple = ["Venkat", 1999, true];
+console.log(myTuple);
---
+++ src/DataTypes.ts
@@ -0,0 +1,24 @@
+//Arrays
+
+let stringArr = ["one", "hey", "Venkat"];
+
+let guitars = ["Strat", "VVSISHH", 5150];
+
+let mixedData = ["EVH", 1984, true];
+
+stringArr.push("Hiii");
+stringArr[0] = "Sundar";
+
+guitars[0] = 1984;
+guitars.unshift("Yamaha");
+
+let test = [];
+let bands: string[] = [];
+bands.push("Glinx");
+
+console.log(guitars);
+
+//Tuples
+
+let myTuple: [string, number, boolean] = ["Venkat", 1999, true];
+console.log(myTuple);
