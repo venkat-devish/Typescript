c836c1a234123820db9dabfc2421679615ef9ee4
Vudutha Venkata Surya kiran <114286141+venkat-devish@users.noreply.github.com>
2023-02-18 23:56:27 +0530

    add:classes

---
+++ src/classes.ts
@@ -0,0 +1,13 @@
+class Coder {
+    name: string;
+    age: number;
+    lang: string;
+    music: string;
+
+    constructor(name: string, age: number, lang: string, music: string) {
+        this.name = name;
+        this.age = age;
+        this.lang = lang;
+        this.music = music;
+    }
+}
