b6376c4971a43c5b1674e8ff0e5fde2c9b047699
Vudutha Venkata Surya kiran <114286141+venkat-devish@users.noreply.github.com>
2023-02-21 08:15:13 +0530

    update: record utility

--- src/indexSignatures.ts
+++ src/indexSignatures.ts
@@ -1,33 +1,33 @@
-interface TransactionObj {
-    readonly [index: string]: number,
+interface TransactionsObj {
+    [index: string]: number,
     Pizza: number,
     Books: number,
     Job: number
 }
 
-const todayTransactions: TransactionObj = {
+const todayTransactions: TransactionsObj = {
     Pizza: -10,
     Books: -10,
-    Job: -10
+    Job: 5
 }
 
 console.log(todayTransactions['Pizza'])
 console.log(todayTransactions.Pizza)
 
-let prop: string = 'Pizza';
-console.log(todayTransactions[prop]);
+let prop: string = 'Pizza'
+console.log(todayTransactions.prop)
 
-const todaysNet = (transactions: TransactionObj) => {
-    let total = 0;
-    for (let key in todayTransactions) {
-        total += todayTransactions[key]
-    }
-    return total;
+for (const key in todayTransactions) {
+    console.log(`${key}: ${todayTransactions[key]}`)
 }
 
-console.log(todaysNet(todayTransactions))
-
-/////////////////////////////////////////////
+const todaysNet = (transactions: TransactionsObj): number => {
+    let total = 0
+    for (const key in todayTransactions) {
+        total += transactions[key]
+    }
+    return total
+}
 
 interface Student {
     // [key: string]: string | number | number[] | undefined,
@@ -38,25 +38,39 @@ interface Student {
 
 const student: Student = {
     name: 'Venkat',
-    GPA: 3.5,
+    GPA: 3.4,
     classes: [100, 200]
 }
-
-// console.log(student[test])
 
 for (const key in student) {
     console.log(`${key}: ${student[key as keyof Student]}`)
 }
 
-Object.keys(student).map(key => {
-    console.log(student[key as keyof typeof student])
-})
+//If we don't know about type of object then we can directly use type of main object itself
 
-const logStudent = (student: Student, key: keyof Student): void => {
+Object.keys(student).map(key => console.log(student[key as keyof typeof student]))
+
+const logStudentKey = (student: Student, key: keyof Student): void => {
     console.log(`Student ${key}: ${student[key]}`)
 }
+logStudentKey(student, 'name')
 
-logStudent(student, 'GPA');
+//////////////////////////////////
+// interface Incomes {
+//     [key: string]: number
+// }
 
-////////////////////////////////////////////
+type streams = 'salary' | 'bonus' | 'sidehustle'
+
+type Incomes = Record<streams, number>
+
+const monthlyIncomes: Incomes = {
+    salary: 1500000,
+    bonus: 100000,
+    sidehustle: 15000
+}
+
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue as keyof Incomes])
+}
 
