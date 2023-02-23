df8a74cc5844c0c1555f66951656e767cb05ba0c
Vudutha Venkata Surya kiran <114286141+venkat-devish@users.noreply.github.com>
2023-02-23 08:54:55 +0530

    add: read only & record

--- src/utility.ts
+++ src/utility.ts
@@ -19,3 +19,43 @@ const updateAssignment = (assign: Assignment, updateProps: Partial<Assignment>):
 
 const assignGraded: Assignment = updateAssignment(assign1, { grade: 95 })
 console.log(assignGraded)
+
+//Required & Readonly
+
+const requiredAssign = (assign: Required<Assignment>): Assignment => {
+    return assign
+}
+
+const assignVerified: Readonly<Assignment> = {
+    ...assignGraded, verified: true
+}
+
+requiredAssign({ ...assignVerified, verified: true })
+
+//Record
+
+const hexColorMap: Record<string, string> = {
+    red: 'FF0000',
+    green: '00FF00',
+    blue: '0000FF'
+}
+
+type Students = 'Venkat' | 'Nagarjuna'
+type Grades = 'A' | 'B' | 'C' | 'D' | 'N'
+
+const finalGrades: Record<Students, Grades> = {
+    Venkat: 'A',
+    Nagarjuna: 'B'
+}
+
+type marks = {
+    assign1: number,
+    assign2: number
+}
+
+const markGrades: Record<Students, marks> = {
+    Venkat: { assign1: 95, assign2: 92 },
+    Nagarjuna: { assign1: 90, assign2: 92 },
+}
+
+//Pick & omit
